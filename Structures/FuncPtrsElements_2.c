af4511d452bb0ff97803ed43e9df12d0c1712d90
Muhammad ALMALKI <58776885+engmalki@users.noreply.github.com>
2025-09-11 07:54:55 +0000

    Create Struct Array with different Operations

--- Structures/FuncPtrsElements_2.c
+++ Structures/FuncPtrsElements_2.c
@@ -1,16 +1,17 @@
 
 #include <stdio.h>
+# define NUMBEROFELEMENTS 3
 typedef struct 
 {
     int x;
     int y;
     int (*op)(int, int);
 }MathOp;
-int sum (int x, int y)
+int Sum (int x, int y)
 {
     return x+y;
 }
-int sub (int x, int y)
+int Sub (int x, int y)
 {
     return x-y;
 }
@@ -21,12 +22,19 @@ int Muliplication (int x, int y)
 
 int main ()
 {
-    MathOp SumOp = {
-        .x=1,
-        .y=2,
-        .op = sum
+    MathOp Ops[NUMBEROFELEMENTS] = {
+        {.x=1,        .y=2,        .op = Sum},
+        {.x=1,        .y=2,        .op = Sub},
+        {.x=1,        .y=2,        .op = Muliplication}
+       
     };
-    int result = SumOp.op (SumOp.x,SumOp.y);
-    printf ("The result=%d\n", result);
+    int result = 0;
+    for (int i=0; i< NUMBEROFELEMENTS; i++)
+    {
+        result = Ops[i].op (Ops[i].x , Ops[i].y);
+        printf ("The result %d= %d\n", i+1, result);
+    }
+
+
     return 0;
 }
