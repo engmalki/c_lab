b978022ead5089c452d51696eb1004a66ad67949
Muhammad ALMALKI <58776885+engmalki@users.noreply.github.com>
2025-09-11 08:13:35 +0000

    Implementing Command Dispatcher

--- Structures/FuncPtrsElements.c
+++ Structures/FuncPtrsElements.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 typedef struct 
 {
     void (*start) (void);
@@ -13,17 +14,28 @@ void StopFunction ()
 {
     printf ("System Stopped.\n");
 }
-
+void executeCommand(CommandHandler *handler, const char * command )
+{
+    if (command == NULL || handler == NULL) return;
+    else if (strcmp (command , "start") == 0 && handler -> start )
+        handler -> start();
+    else if (strcmp (command, "stop") == 0 && handler ->stop)
+        handler-> stop();
+    else
+        printf("Unknown Command %s\n", command );
+    
+}
 int main ()
 {
     CommandHandler CmdHnd1 = {
         .start = StartFunction,
         .stop = StopFunction,
     };
-    
-    CmdHnd1.start();
+    executeCommand(&CmdHnd1, "start"); 
     printf (". . . \n");
-    CmdHnd1.stop();
+    executeCommand(&CmdHnd1, "123");
+    printf (". . . \n");
+    executeCommand(&CmdHnd1, "stop");
  
     return 0;
 }
